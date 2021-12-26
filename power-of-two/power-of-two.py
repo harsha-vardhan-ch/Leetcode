e1c24f5f962446cef89b0c8b1aa36f2545610710
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2021-12-25 22:58:04 -0800

    Time: 32 ms (63.32%), Space: 14 MB (98.08%) - LeetHub

---
+++ power-of-two/power-of-two.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if(n==0):
+            return False
+        while(n%2==0):
+            n/=2
+        return n==1
+        
