033c368ad2573aa998f2373d3cf3f97890887e51
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-01-21 18:50:22 -0800

    Time: 1550 ms (25.25%), Space: 25.1 MB (82.81%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,7 +4,5 @@ class Solution:
         minPurchase = prices[0]
         for i in range(1, len(prices)):		
             maxProfit = max(maxProfit, prices[i] - minPurchase)
-            print(minPurchase,'    f')
             minPurchase = min(minPurchase, prices[i])
-            print(minPurchase)
         return maxProfit
