7033d88f5207f8dd7942b9881b65a48ead5f3457
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-02-14 10:54:09 -0800

    Time: 233 ms (46.13%), Space: 14.4 MB (82.91%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -4,19 +4,11 @@ class Solution:
         c=0
         boxTypes.sort(key=lambda x: x[1], reverse=True)
         for i in boxTypes:
-            print(i[0])
             if i[0]<=t:
-                print(i[0], '    i[0]')
                 t=t-i[0]
                 c=c+ i[0]*i[1]
-                print(c,'   c')
             else:
-                print(i[0],i[1],t)
                 c=c+ (t)*i[1]
-                t=0
-                print(c, 'cc',t)
-                if t<=0:
-                    return c
-             
+                return c 
         return c
     
