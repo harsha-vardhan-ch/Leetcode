f31060de6697fe007395e785a75d0e29ab93302c
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-02-14 11:21:00 -0800

    Time: 47 ms (54.31%), Space: 14 MB (82.73%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -3,9 +3,8 @@ class Solution:
         for c in "!?',;.": 
             para = para.replace(c, " ")
         par=para.lower()
-        print(par)
+        #print(par)
         d=Counter(par.split(' ')).most_common()
-        print(d)
         for i in d:
             if i[0]!='' and i[0] not in banned :
                 return i[0]
