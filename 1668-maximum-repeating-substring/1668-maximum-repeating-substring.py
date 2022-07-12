1f203cca67a6ad47df81140f881982e10aa73983
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-07-12 15:00:38 -0700

    Time: 49 ms (51.16%), Space: 13.8 MB (62.46%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        '''
+        App 1 - Bruteforce
+        '''
+        count = 0
+        while word * (count + 1) in sequence:
+            count +=  1        
+        return count
+    
+        '''
+        App 2 - Through binary search
+        
+        if word not in sequence:
+            return 0
+
+        left = 1
+        right = len(sequence) // len(word)
+        while left <= right:
+            mid = (left + right) // 2
+            if word * mid in sequence:
+                left = mid + 1
+            else:
+                right = mid - 1 
+                
+        return left - 1
+        '''
