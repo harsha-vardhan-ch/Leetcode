13903c3365835d5d7c7624d0d0658c778b7cbb08
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-07-11 18:36:25 -0700

    Time: 96 ms (39.50%), Space: 14.1 MB (12.76%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minMaxGame(self, nums: List[int]) -> int:
+        n=len(nums)
+        while n!=1:
+            ch=True
+            i=0
+            while i<=(n//2)-1:
+                if ch:
+                    if nums[i]>nums[i+1]:
+                        del nums[i]
+                    else:
+                        del nums[i+1]
+                    # del max(nums[i],nums[i+1])
+                    ch=False
+                else:
+                    if nums[i]>nums[i+1]:
+                        del nums[i+1]
+                    else:
+                        del nums[i]
+                    # del min(nums[i],nums[i+1])
+                    ch=True
+                i+=1
+            print(nums)
+            n=len(nums)
+            
+        return nums[0]
