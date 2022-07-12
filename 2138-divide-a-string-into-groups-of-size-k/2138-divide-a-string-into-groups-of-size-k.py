84c44875e86bf6c39adffe19f9c7cea6e499764c
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-07-12 14:31:08 -0700

    Time: 59 ms (25.99%), Space: 13.9 MB (74.67%) - LeetHub

---
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.py
@@ -0,0 +1,9 @@
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        if len(s)%k!=0:
+            s=s+ fill * (k-(len(s)%k)) 
+        h = [s[i:i+k] for i in range(0,len(s),k)]
+        # for i in s:
+            
+        # print(h)
+        return h
