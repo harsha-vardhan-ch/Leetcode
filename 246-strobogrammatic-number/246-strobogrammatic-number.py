8707f2d12ff9e3e0b023c4fff43b56d4093021cc
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-07-20 15:11:57 -0700

    Time: 30 ms (93.25%), Space: 13.9 MB (11.29%) - LeetHub

---
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        sing = ['1','0','8']
+        if len(num)<2:
+            if num=='0' or num=='8' or num=='1':
+                return True
+            return False
+        i=0
+        j=len(num)-1
+        d = { '0':'0', '6':'9', '8':'8', '9':'6', '1':'1'}
+        while i<j:
+            if num[i] not in d:
+                return False
+            if d[num[i]] == num[j]:
+                i+=1
+                j-=1
+            else:
+                return False
+        if num[i]==num[j] and num[i] in sing:
+            return True
+        elif num[i]==num[j] and num[i] not in sing:
+            return False
+        return True
+            
