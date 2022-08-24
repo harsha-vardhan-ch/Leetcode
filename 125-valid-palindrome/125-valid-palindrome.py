0c6fc5cd4fc02670d1b1f2673a73f2f4b171bf7c
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-08-23 17:29:10 -0700

    Time: 85 ms (37.60%), Space: 14.4 MB (57.17%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,33 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        '''
-        M1 
-        '''
-#         temp=''
-#         for i in s:
-#             if i.isalnum():
-#                 temp+=i.lower()
-#         # print(temp)
-#         return temp[::-1]==temp
-    
-        '''
-        M 2
-        '''
         l=0
         r=len(s)-1
-        while l<r:
-            while l<r and not s[l].isalnum():
-                l+=1
-            while r>l and not s[r].isalnum():
-                r-=1
-            if s[l].lower()!=s[r].lower():
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
                 return False
-            
-            l+=1
-            r-=1
+
+            l += 1
+            r -= 1
+
         return True
-    
-        '''
-        T.C - same
-        Memory - Efficent that M1
-        '''
