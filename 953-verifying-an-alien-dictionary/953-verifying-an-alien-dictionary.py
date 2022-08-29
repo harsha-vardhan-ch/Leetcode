cc3034879b4d34960c02527e0e89b9126e1dbeee
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-08-29 13:45:30 -0700

    Time: 78 ms (11.68%), Space: 13.8 MB (83.38%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = {}
+        for index, val in enumerate(order):
+            order_map[val] = index
+        # print(order_map)
+        for i in range(len(words)-1):
+            # t=min(len(words[i]),len(words[i+1]))
+            for j in range(len(words[i])):
+                # For cases like "apple", "app"
+                if j>=len(words[i+1]):
+                    return False
+                if words[i][j]!=words[i+1][j]:
+                    # if greater => not sorted, so return False
+                    if order_map[words[i][j]]>order_map[words[i+1][j]]:
+                        return False
+                    # if less than => in sorted order [ < ] , no further checking needed
+                    break
+        return True
+            
+            
