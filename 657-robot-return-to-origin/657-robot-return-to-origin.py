cf9f2d79ee29001d27bbbc408a9e87188c6efc5e
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-08-29 12:28:25 -0700

    Time: 120 ms (18.16%), Space: 16.6 MB (38.33%) - LeetHub

--- 657-robot-return-to-origin/657-robot-return-to-origin.py
+++ 657-robot-return-to-origin/657-robot-return-to-origin.py
@@ -1,11 +1,5 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        def check(x,y):
-            for i in ar:
-                # print(i[0],i[1],x,y)
-                if x==i[0] and y==i[1] and x==0 and y==0:
-                    return True
-            return False
         x=0
         y=0
         ar=[(0,0)]
@@ -18,11 +12,8 @@ class Solution:
                 x-=1  
             elif i=='D':
                 y-=1
-            # print(x,y)
             ar.append((x,y)) 
-        # print(ar[-1][0])
         if ar[-1][0]==0 and ar[-1][1]==0:
-            return True
-            
+            return True   
         return False
         
