01276e55b34b6129dbc57870a2f14a369912a4be
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-08-30 18:53:29 -0700

    Time: 56 ms (22.41%), Space: 13.8 MB (55.31%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.py
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.py
@@ -1,7 +1,34 @@
 class Solution:
     def removeVowels(self, s: str) -> str:
-        res=''
+        '''
+        App 1
+        Since, strings are immutable and creating them requires allocating new memory, this method is inefficient.
+        '''
+        # res=''
+        # for i in s:
+        #     if i not in "aeiou":
+        #         res+=i
+        # return res
+        
+        '''
+        App 2
+        Allocating memory only for final string. But, if i not in 'aeiou' taken O(5) lookup. 
+        '''
+        # res=[]
+        # for i in s:
+        #     if i not in "aeiou":
+        #         res.append(i)
+        # return "".join(res)
+    
+        '''
+        App 3.
+        
+        Hashtable/hashset gives O(1) lookup.
+        '''
+        res=[]
+        vowels=set('aeiou')
         for i in s:
-            if i not in "aeiou":
-                res+=i
-        return res
+            if i not in vowels:
+                res.append(i)
+        return "".join(res)
+        
