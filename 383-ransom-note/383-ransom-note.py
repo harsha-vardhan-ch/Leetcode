dfb41e470565033f2ec5e0292fdf7350b6e68cee
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-08-30 19:49:25 -0700

    Time: 59 ms (87.51%), Space: 14.2 MB (18.55%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,9 +1,33 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for i in ransomNote:
-            r=magazine.find(i)
-            if r<0:
+        '''
+        App 1
+        m = len(magazie)
+        n=len(ransom)
+        T.C - O(m.n)
+        S.C - O(m)
+        '''
+        # for i in ransomNote:
+        #     r=magazine.find(i)
+        #     if r<0:
+        #         return False
+        #     else:
+        #         magazine=magazine[:r]+magazine[r+1:]
+        # return True
+    
+        '''
+        App 2
+        '''
+        if len(ransomNote) > len(magazine):
+            return False
+
+        magazine_counts = collections.Counter(magazine)
+        ransom_note_counts = collections.Counter(ransomNote)
+
+        for char, count in ransom_note_counts.items():
+            magazine_count = magazine_counts[char]
+            if magazine_count < count:
                 return False
-            else:
-                magazine=magazine[:r]+magazine[r+1:]
+
+        
         return True
