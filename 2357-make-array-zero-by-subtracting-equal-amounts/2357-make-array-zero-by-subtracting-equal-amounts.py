d1c599ac41a4ea524d7affa253eaddea4f381b06
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-09-01 16:18:10 -0700

    Time: 51 ms (66.20%), Space: 13.8 MB (58.26%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        '''
+        App 1
+        Write logic
+        T.C - O(n^2)
+        
+        '''
+        '''
+        App 2
+        Sort, Logic
+        '''
+        
+        '''
+        App 3
+        Sets
+        Number of unique non zero elements in array
+        '''
+        
+        return len(set(nums)-{0})
+        
