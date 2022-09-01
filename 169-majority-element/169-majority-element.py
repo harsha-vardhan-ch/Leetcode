466382c81fc3c31d575cf08342d420920e494db6
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-09-01 16:42:41 -0700

    Time: 440 ms (5.04%), Space: 15.5 MB (85.89%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,4 +1,42 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return Counter(nums).most_common(1)[0][0]
+        # return Counter(nums).most_common(1)[0][0]
+    
+        '''
+        App 1
+
+        Counter 
+
+        T.C - O(n)
+        S.C - O(n)
+        '''
+
+        '''
+        App 2
+
+        Sort and return middle lemeent
+        T.C - O(nlogn)
+        S.C - O(1)
+
+        '''
+
+        '''
+        App 3
+
+        Boyer moore voting algorithm
+        T.C - O(n)
+        S.C -O(1)
+
+        '''
+        can=0
+        c=0
+        for i in nums:
+            if c==0:
+                can=i
+            if can!=i:
+                c=c-1
+            else:
+                c=c+1
+        return can
+            
         
