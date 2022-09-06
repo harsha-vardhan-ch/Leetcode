707f5dc9bd9aac4c8a4519263178b9d32251951b
Harsha Vardhan <36564415+harsha-vardhan-ch@users.noreply.github.com>
2022-09-05 20:59:57 -0700

    Time: 45 ms (57.51%), Space: 14 MB (21.77%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -1,24 +1,5 @@
 class Solution:
     def isRobotBounded(self, ins: str) -> bool:
-#         insu=''
-#         for i in ins:
-#             if i != 'G':
-#                 insu+=i
-#         if insu == "":
-#             return False
-#         if len(insu) == 1 and (insu == 'L' or insu=='R'):
-#             return True
-#         for i in range(len(insu)-1):
-#             if (insu[i]=='L' and insu[i+1]=='L') or (insu[i]=='R' and insu[i+1]=='R'):
-#                 return True
-            
-#         return False
-        # def check(x,y):
-        #     for i in ar:
-        #         # print(i[0],i[1],x,y)
-        #         if x==i[0] and y==i[1]:
-        #             return True
-        #     return False
         def chgdir(face,inst):
             right={'W':'N','N':'E','E':'S','S':'W'}
             left={'W':'S','S':'E','E':'N','N':'W'}
@@ -29,19 +10,13 @@ class Solution:
                     
         face='N'
         n=len(ins)
-        # h='L' not in instructions
-        # v='R' not in instructions
-        # print(h,v) 
         if 'L' not in ins and 'R' not in ins:
             return False
         x=0
         y=0
-        ar=[(0,0)]
+        # ar=[(0,0)]
         i=0
-        # suck=0
         while i<len(ins):
-            
-            
             if ins[i] == 'G':
                 if face == 'N':
                     y+=1
@@ -51,22 +26,13 @@ class Solution:
                     x-=1  
                 elif face=='S':
                     y-=1
-                # if check(x,y):
-                #     suck=1
                 if i == (n-1) and ((x==0 and y==0) or face != 'N'):
-                    # print('hv')
                     return True
-                ar.append((x,y))         
+                # ar.append((x,y))         
             else:
                 face = chgdir(face,ins[i])
                 if i == (n-1) and ((x==0 and y==0) or face != 'N'):
                     return True
-            # print(x,y)
-            # print(ins[i],ar,face)
             i+=1
-            # if suck==0 and i==n:
-            #     i=0
-            
-        # print(ar)
         return False
         
